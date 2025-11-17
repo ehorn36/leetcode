5e584e0d64ca1c4468674a814e888920896023da
ehorn36 <ehorn36@gmail.com>
2025-11-17 18:21:01 -0500

    Adding LC binary search answer

---
+++ binary_search/LC_704_binary_search.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+
+        # Set two pointers
+        left = 0
+        right = len(nums) - 1
+
+        # Continue until left pointer cross right pointer
+        while left <= right:
+            middle = (left + right) // 2
+
+            if nums[middle] > target:
+                right = middle - 1
+            elif nums[middle] < target:
+                left = middle + 1
+
+            # Middle must == target
+            else:
+                return middle
+
+        # If target not found
+        return -1
+
+
+nums = [0, 1, 2, 3, 4]
+target = 1
+
+test = Solution()
+print(test.search(nums, target))
